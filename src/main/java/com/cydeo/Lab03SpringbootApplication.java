75c273f196b541164cd9d8e911709fdf7fa46ab1
naranburulov <naranburulov@gmail.com>
2022-09-23 09:10:16 -0400

    lab03 completed and tested

--- src/main/java/com/cydeo/Lab03SpringbootApplication.java
+++ src/main/java/com/cydeo/Lab03SpringbootApplication.java
@@ -1,13 +1,17 @@
 package com.cydeo;
 
+import com.cydeo.service.RecipeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lab03SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Lab03SpringbootApplication.class, args);
+        ApplicationContext context = SpringApplication.run(Lab03SpringbootApplication.class, args);
+        RecipeService recipeService = context.getBean(RecipeService.class);
+        recipeService.prepareRecipe();
     }
 
 }
